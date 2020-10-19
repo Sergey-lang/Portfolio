61a99632fdb4a9b33a4266f164e66b4e71ceb959
Sergey-lang <sergei.kuharyonok@yandex.by>
2020-10-20 00:12:48 +0300

    create Header,add css comb

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import s from './Header.mosule.css'
+
+export function Header() {
+    return (
+        <div className={s.header}>
+
+        </div>
+    );
+}
+
