7613671355bc905999bcfd1377af349d4084b576
Sergey-KA <sergei.kuharyonok@yandex.by>
2020-10-21 15:36:11 +0300

    Updates

---
+++ precache-manifest.ac5c86b43024add388a0cfa1d7cf9983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5852dcd94a7227e1072d39fcebc1b36",
+    "url": "/My_portfolio/index.html"
+  },
+  {
+    "revision": "7474766a1372a6a0ac45",
+    "url": "/My_portfolio/static/css/main.dd43ef32.chunk.css"
+  },
+  {
+    "revision": "0314f5db920723e4d5ed",
+    "url": "/My_portfolio/static/js/2.6d320f2b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/My_portfolio/static/js/2.6d320f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7474766a1372a6a0ac45",
+    "url": "/My_portfolio/static/js/main.4f2cc1af.chunk.js"
+  },
+  {
+    "revision": "a865e1d0b6f43199bd26",
+    "url": "/My_portfolio/static/js/runtime-main.04439d10.js"
+  }
+]);
